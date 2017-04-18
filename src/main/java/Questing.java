b59f6b8a8044f0b50917b900f3a6554eef67591c
marushru <marushru@naver.com>
2017-04-18 15:08:29 +0900

    test

---
+++ src/main/java/Questing.java
@@ -0,0 +1,18 @@
+/**
+ * 테스트 코드를 보고, 아래 두 method 를 작성해주세요.
+ */
+class Questing {
+	static int quest1(double d)  {
+		// FIXME
+	    int i = (int)d;
+	    int result = i/2;
+		return result;
+	}
+
+	static boolean quest2(String s) {
+		// FIXME
+	    String a = "({}[])";
+	    boolean result = a.equals(s);
+		return result;
+	}
+}
